9ac9badc6e3aae1d9f1ed6471f06bfe8c99fcb7a
unknown <shirelpinker@gmail.com>
2022-07-23 23:50:36 +0300

    responsive navbar and header

--- client/src/app/components/categories/categories.component.ts
+++ client/src/app/components/categories/categories.component.ts
@@ -1,7 +1,9 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Observable, of } from 'rxjs';
 import { Category } from 'src/app/models/category';
 import { CategoriesService } from 'src/app/services/categories.service';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-categories',
@@ -12,14 +14,31 @@ export class CategoriesComponent implements OnInit {
   categories$: Observable<Category[]> = of([]);
   @Output() categorySelected = new EventEmitter();
   @Input() activeCategory: number | null;
+  searchWord: FormControl;
 
-  constructor(private categoriesService: CategoriesService) { }
+
+
+  constructor(private categoriesService: CategoriesService, private productsService: ProductsService) { }
 
   ngOnInit(): void {
+    this.searchWord = new FormControl(null);
     this.categories$ = this.categoriesService.getCategories();
+    this.onCategoryClicked(1);
   }
+
   onCategoryClicked(categoryId) {
-    this.categorySelected.emit(categoryId);
+    this.activeCategory = categoryId;
+    this.searchWord.setValue('')
+    this.productsService.getProductsByCategoryId(this.activeCategory).subscribe()
+  }
+
+  searchProduct() {
+    this.activeCategory = null;   
+    this.productsService.getProductsBySearchName(this.searchWord.value || "").subscribe();
+  }
+
+  onSearchWordBlur(){
+    this.searchWord.setValue(null)
   }
 
 }
--- client/src/app/components/shopping-page/shopping-page.component.ts
+++ client/src/app/components/shopping-page/shopping-page.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { Mode } from 'src/app/models/modeEnum';
-import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-shopping-page',
@@ -16,35 +14,15 @@ export class ShoppingPageComponent implements OnInit {
   isShown = true;
   mode: Mode;
   Mode = Mode;
-  searchWord: FormControl;
   activeCategory: number | null;
 
-  constructor(private productsService: ProductsService, private activatedroute: ActivatedRoute) { }
+  constructor(private activatedroute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.searchWord = new FormControl(null);
     this.activatedroute.data.subscribe(data => { this.mode = data['mode']; })
-    this.onCategorySelected(1);
-  }
-
-  onCategorySelected(categoryId) {
-    this.activeCategory = categoryId;
-    this.searchWord.setValue('')
-    this.productsService.getProductsByCategoryId(this.activeCategory).subscribe()
-  }
-
-  searchProduct() {
-    
-    this.activeCategory = null;   
-    this.productsService.getProductsBySearchName(this.searchWord.value || "").subscribe();
-  }
-
-  onSearchWordBlur(){
-    this.searchWord.setValue(null)
   }
 
   toggleShow() {
     this.isShown = !this.isShown;
   }
-
 }
